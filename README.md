2c77847ac1c9de13a352b48684fc6dc49097de95
Ravi Wu <raviwu@gmail.com>
2019-03-05 13:31:44 +0800

    Configure formatting tool

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 A simple SSL context helper that can import custom pem keys in addition to default key store. Inspired by [this stackoverflow discussion](https://stackoverflow.com/questions/24555890/using-a-custom-truststore-in-java-as-well-as-the-default-one).
 
+## Usage
+
+Compile, test, build, and code analysis:
+
+```
+mvn clean verify
+```
--- src/test/java/org/lwstudio/AppTest.java
+++ src/test/java/org/lwstudio/AppTest.java
@@ -1,20 +1,11 @@
 package org.lwstudio;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+  @Test
+  public void shouldAnswerWithTrue() {
+    Assert.assertTrue(true);
+  }
 }
